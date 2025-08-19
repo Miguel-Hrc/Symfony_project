08f195384b42d6a997293f31ae005051690087d1
Miguel-Hrc <136330905+Miguel-Hrc@users.noreply.github.com>
2025-08-19 21:01:01 +0400

    Update bundles.php

--- config/bundles.php
+++ config/bundles.php
@@ -2,16 +2,20 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
+
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ($_ENV['USE_MONGODB'] ?? 'false') !== 'true' ? ['all' => true] : [],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ($_ENV['USE_MONGODB'] ?? 'false') !== 'true' ? ['all' => true] : [],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ($_ENV['USE_MONGODB'] ?? 'false') !== 'true' ? ['prod' => true,'dev' => true, 'test' => true] : [],
+
+    Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ($_ENV['USE_MONGODB'] ?? 'false') === 'true' ? ['all' => true] : [],
+
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['prod' => true,'dev' => true, 'test' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['prod' => true,'dev' => true, 'test' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['prod' => true,'dev' => true],
 ];
